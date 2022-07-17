a96714d60001c43344bc3f5173fb0175d15abcc6
Lucas Colombo Hurpia dos Santos <79328641+LucasColomboHurpia@users.noreply.github.com>
2022-07-17 01:58:24 -0700

    Add files via upload

--- Scripts/index.js
+++ Scripts/index.js
@@ -99,7 +99,7 @@ const display = () =>{
     lastSeen.innerHTML = lastVisitedTools
 
     //popular tools
-    for (let index = 0; index < newTool.length; index++) {
+    for (let index = 0; index < 8; index++) {
         
     let newTools =
        `
@@ -126,7 +126,7 @@ const display = () =>{
     let moreTools = `
     <div class="row mb-3">
     <div class="c-5 mt-3">
-        <div class="moreTools" onclick="alertComingSoon()">
+        <div class="moreTools" onclick="showMoreTools()">
             <div class="card-horizontal">
                 <div class="card-body">
                     <img src="https://cdn-icons-png.flaticon.com/512/32/32339.png" 
@@ -251,3 +251,62 @@ const showToolPage = (id) => {
 const alertComingSoon = () =>{
     alert("Feature coming soon!")
 }
+
+const showMoreTools = () =>{
+
+    //changes css
+    document.getElementById('stylesheet').href='././Styles/moreTools.css';
+    
+        displayArea.innerHTML = `
+        <div class="container py-3 h-100">
+        <div class="row d-flex justify-content-center align-items-center h-100">
+            <div class="col col-lg-9 col-xl-7">
+                <div class="card">
+    
+                    <div class="p-4 card text-black" style="background-color: #f8f9fa;">
+                    <div class="returnButton" onclick="display()"> <img
+                    src="https://icons.veryicon.com/png/o/miscellaneous/arrows/go-back-2.png"
+                    width="20px" alt=""> Return</div>
+                                    
+                        <!-- Popular Tools -->
+                        <div class="lastseen-txt mt-4 h5">Design and Developer tools</div>
+    
+                        <div id="listOfTool">
+                            <div class="container-fluid PopularTools">
+                                <!-- js goes here -->
+                            </div>
+                        </div>
+    
+    
+                    </div>
+                </div>
+            </div>
+        </div>
+    
+    
+    `
+    
+    for (let index = 0; index < newTool.length; index++) {
+            
+        let newTools =
+           `
+           <div class="row">
+           <div class="c-5 mt-3">
+               <div class="cardPopular" >
+                   <div class="card-horizontal">
+                       <div class="card-body" onclick="showToolPage(${newTool[index].id})">
+                           <img src="${newTool[index].icon}" class="img-fluid popularListIMG" alt="Card image cap"> 
+                           <div class="popularToolName">${newTool[index].language}</div>
+                       </div>
+                   </div>
+                   <div class="card-footer">
+                       <p>${newTool[index].desc}</p>
+                   </div>
+               </div>
+           </div>
+       </div>
+           ` 
+           $(".PopularTools").append(newTools)       
+        }
+    
+    } 
